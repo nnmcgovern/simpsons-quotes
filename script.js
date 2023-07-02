73b04254f56b0585f4d531964df21e49db5e9607
Natalie Nicole <natalienicole@MacBook-Pro.local>
2023-07-02 15:52:30 -0400

    working basic modal

--- script.js
+++ script.js
@@ -12,11 +12,27 @@ function handleClick(e) {
 
 const baseURL = "https://thesimpsonsquoteapi.glitch.me/quotes"
 const count = 1
+const modal = document.querySelector(".modal")
+const buttonClose = document.querySelector(".close")
+const buttonAnswer = document.querySelector(".show-answer")
+const buttonNext = document.querySelector(".next-quote")
+
+buttonAnswer.addEventListener("click", handleClickAnswer)
+buttonClose.addEventListener("click", handleClickAnswer)
 
 fetch(`${baseURL}?count=${count}`)
   .then(res => res.json())
   .then(data => {
     const paragraph = document.querySelector(".quote")
+    const charIMG = document.querySelector(".char-img")
+    const charName = document.querySelector(".char-name")
+
     paragraph.innerHTML = `"${data[0]["quote"]}"`
+    charIMG.style.backgroundImage = `url(${data[0]["image"]})`
+    charName.innerHTML = data[0]["character"]
   })
   .catch(err => console.log(err))
+
+function handleClickAnswer(e) {
+  modal.classList.toggle("hidden")
+}
